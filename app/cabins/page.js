cc70d4cd47760ce29bf62ba255f100bcb86ae805
ptlnik13 <nikeshvpatel@gmail.com>
2026-02-10 12:21:53 -0500

    Building the server component with fetch data within sc

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,8 +1,16 @@
 import React from 'react';
 
-function Page(props) {
+async function Page(props) {
+    const res = await fetch('https://jsonplaceholder.typicode.com/users');
+    const data = await res.json();
+
     return (
-        <h1>Cabins Page</h1>
+        <div>
+            <h1>Cabins Page</h1>
+            <ul>
+                {data.map(user => <li key={user.id}>{user.name}</li>)}
+            </ul>
+        </div>
     );
 }
 
