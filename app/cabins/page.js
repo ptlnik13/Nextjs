dc916de06e6c96a8bfe95ab5492f80bc322038f2
ptlnik13 <nikeshvpatel@gmail.com>
2026-02-10 14:34:27 -0500

    Styling done

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,17 +1,10 @@
-import Counter from "@/app/components/counter";
+import Counter from "@/app/_components/Counter";
 
-async function Page(props) {
-    const res = await fetch('https://jsonplaceholder.typicode.com/users');
-    const data = await res.json();
+function Page(props) {
 
     return (
         <div>
             <h1>Cabins Page</h1>
-            <ul>
-                {data.map(user => <li key={user.id}>{user.name}</li>)}
-            </ul>
-
-            <Counter users={data}/>
         </div>
     );
 }
