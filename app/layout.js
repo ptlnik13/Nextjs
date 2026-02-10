4a4351a1dc099c1f4e0bae42f6271055d4098547
ptlnik13 <nikeshvpatel@gmail.com>
2026-02-10 16:25:12 -0500

    Improving nav and routes

--- app/layout.js
+++ app/layout.js
@@ -1,10 +1,16 @@
-import Navigation from "@/app/_components/Navigation";
-import Logo from "@/app/_components/Logo";
+import Header from "@/app/_components/Header";
+import {Josefin_Sans} from "next/font/google";
+
+
+const josefin = Josefin_Sans({
+    display: 'swap',
+    subsets: ['latin']
+})
 
 import "@/app/_styles/globals.css";
 
 export const metadata = {
-    title: {
+    title      : {
         template: "%s / The Wild Oasis",
         default : "Welcome / The Wild Oasis"
     },
@@ -15,14 +21,14 @@ export const metadata = {
 export default function RootLayout({children}) {
     return (
         <html lang="en">
-        <body className='bg-primary-950 text-primary-100 min-h-screen'>
-        <header>
-            <Logo/>
-            <Navigation/>
-        </header>
-        <main>
-            {children}
-        </main>
+        <body className={` ${josefin.className} antialiased bg-primary-950 text-primary-100 min-h-screen flex flex-col`}>
+        <Header/>
+        <div className='flex-1 px-8 py-12'>
+            <main className='max-w-7xl mx-auto'>
+                {children}
+            </main>
+        </div>
+
         </body>
         </html>
     )
