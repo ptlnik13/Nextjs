4a5cfd9ed8f52cb167ad5d26e738c6883377adc9
ptlnik13 <nikeshvpatel@gmail.com>
2026-02-11 13:45:48 -0500

    Putting suspense for more User friendly touch

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,13 +1,13 @@
-import CabinCard from "@/app/_components/CabinCard";
-import {getCabins} from "@/app/_lib/data-service";
+import CabinList from "@/app/_components/CabinList";
+import {Suspense} from "react";
+import Spinner from "@/app/_components/Spinner";
 
 export const metadata = {
     title: 'Cabins'
 }
 
-async function Page(props) {
+function Page(props) {
 
-    const cabins = await getCabins();
     return (
         <div>
             <h1 className="text-4xl mb-5 text-accent-400 font-medium">
@@ -18,18 +18,14 @@ async function Page(props) {
                 Cozy yet luxurious cabins, located right in the heart of the Italian
                 Dolomites. Imagine waking up to beautiful mountain views, spending your
                 days exploring the dark forests around, or just relaxing in your private
-                hot tub under the stars. Enjoy nature's beauty in your own little home
+                hot tub under the stars. Enjoy nature&#39;s beauty in your own little home
                 away from home. The perfect spot for a peaceful, calm vacation. Welcome
                 to paradise.
             </p>
 
-            {cabins.length > 0 && (
-                <div className="grid sm:grid-cols-1 md:grid-cols-2 gap-8 lg:gap-12 xl:gap-14">
-                    {cabins.map((cabin) => (
-                        <CabinCard cabin={cabin} key={cabin.id} />
-                    ))}
-                </div>
-            )}
+            <Suspense fallback={<Spinner/>}>
+                <CabinList/>
+            </Suspense>
         </div>
     );
 }
