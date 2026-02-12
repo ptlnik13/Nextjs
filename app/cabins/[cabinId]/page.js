4e1a2bdc8ad6ad522bfed9cd24a26b97868cd3af
ptlnik13 <nikeshvpatel@gmail.com>
2026-02-12 11:48:03 -0500

    converting some ssc to csc

--- app/cabins/[cabinId]/page.js
+++ app/cabins/[cabinId]/page.js
@@ -1,9 +1,10 @@
-import { EyeSlashIcon, MapPinIcon, UsersIcon } from "@heroicons/react/24/solid";
+import {EyeSlashIcon, MapPinIcon, UsersIcon} from "@heroicons/react/24/solid";
 import {getCabin, getCabins} from "@/app/_lib/data-service";
 import Image from "next/image";
+import TextExpander from "@/app/_components/TextExpander";
 
 export async function generateMetadata({params}) {
-    const { cabinId } = params;
+    const {cabinId} = params;
     const cabin = await getCabin(cabinId);
     return {
         title: `Cabin ${cabin.name}`
@@ -12,19 +13,19 @@ export async function generateMetadata({params}) {
 
 export async function generateStaticParams() {
     const cabins = await getCabins();
-    return cabins.map(cabin => ({ cabinId: String(cabin.id) }));
+    return cabins.map(cabin => ({cabinId: String(cabin.id)}));
 }
 
 export default async function Page({params}) {
-    const { cabinId } = params;
+    const {cabinId} = params;
 
-    const { id, name, maxCapacity, regularPrice, discount, image, description } = await getCabin(cabinId);
+    const {id, name, maxCapacity, regularPrice, discount, image, description} = await getCabin(cabinId);
 
     return (
         <div className="max-w-6xl mx-auto mt-8">
             <div className="grid grid-cols-[3fr_4fr] gap-20 border border-primary-800 py-3 px-10 mb-24">
                 <div className="relative scale-[1.15] -translate-x-3">
-                    <Image fill src={image} className='object-cover' alt={`Cabin ${name}`} />
+                    <Image fill src={image} className='object-cover' alt={`Cabin ${name}`}/>
                 </div>
 
                 <div>
@@ -32,25 +33,29 @@ export default async function Page({params}) {
                         Cabin {name}
                     </h3>
 
-                    <p className="text-lg text-primary-300 mb-10">{description}</p>
+                    <p className="text-lg text-primary-300 mb-10">
+                        <TextExpander>
+                            {description}
+                        </TextExpander>
+                    </p>
 
                     <ul className="flex flex-col gap-4 mb-7">
                         <li className="flex gap-3 items-center">
-                            <UsersIcon className="h-5 w-5 text-primary-600" />
+                            <UsersIcon className="h-5 w-5 text-primary-600"/>
                             <span className="text-lg">
                 For up to <span className="font-bold">{maxCapacity}</span>{" "}
                                 guests
               </span>
                         </li>
                         <li className="flex gap-3 items-center">
-                            <MapPinIcon className="h-5 w-5 text-primary-600" />
+                            <MapPinIcon className="h-5 w-5 text-primary-600"/>
                             <span className="text-lg">
                 Located in the heart of the{" "}
                                 <span className="font-bold">Dolomites</span> (Italy)
               </span>
                         </li>
                         <li className="flex gap-3 items-center">
-                            <EyeSlashIcon className="h-5 w-5 text-primary-600" />
+                            <EyeSlashIcon className="h-5 w-5 text-primary-600"/>
                             <span className="text-lg">
                 Privacy <span className="font-bold">100%</span> guaranteed
               </span>
