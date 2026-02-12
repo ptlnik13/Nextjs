b79fb03eb37806ecb9d78fee22f9fe95d9c8eabb
ptlnik13 <nikeshvpatel@gmail.com>
2026-02-12 14:51:13 -0500

    Context API for date management for reservation

--- app/_components/ReservationForm.js
+++ app/_components/ReservationForm.js
@@ -1,4 +1,9 @@
+'use client'
+import {useReservation} from "@/app/_lib/ReservationContext";
+
 function ReservationForm({cabin}) {
+
+  const {range} = useReservation();
   // CHANGE
   const {maxCapacity} = cabin;
 
--- app/layout.js
+++ app/layout.js
@@ -8,6 +8,7 @@ const josefin = Josefin_Sans({
 })
 
 import "@/app/_styles/globals.css";
+import {ReservationProvider} from "@/app/_lib/ReservationContext";
 
 export const metadata = {
     title      : {
@@ -21,11 +22,14 @@ export const metadata = {
 export default function RootLayout({children}) {
     return (
         <html lang="en">
-        <body className={` ${josefin.className} antialiased bg-primary-950 text-primary-100 min-h-screen flex flex-col relative`}>
+        <body
+            className={` ${josefin.className} antialiased bg-primary-950 text-primary-100 min-h-screen flex flex-col relative`}>
         <Header/>
         <div className='flex-1 px-8 py-12 grid'>
             <main className='max-w-7xl mx-auto w-full'>
-                {children}
+                <ReservationProvider>
+                    {children}
+                </ReservationProvider>
             </main>
         </div>
 
