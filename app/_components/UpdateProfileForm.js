7064409374a984ba30e09643113a200e8fd2442e
ptlnik13 <nikeshvpatel@gmail.com>
2026-02-13 16:22:51 -0500

    Update reservation functions

--- app/_components/UpdateProfileForm.js
+++ app/_components/UpdateProfileForm.js
@@ -1,10 +1,7 @@
-'use client'
-import React, {useState} from 'react';
 import {updateGuestAction} from "@/app/_lib/actions";
-import { useFormStatus} from "react-dom";
+import SubmitButton from "@/app/_components/SubmitButton";
 
-function UpdateProfileForm({ children, guest}) {
-    const [count, setCount] = useState(0)
+function UpdateProfileForm({children, guest}) {
     const {fullName, email, nationality, nationalID, countryFlag} = guest;
 
     return (
@@ -51,22 +48,13 @@ function UpdateProfileForm({ children, guest}) {
             </div>
 
             <div className="flex justify-end items-center gap-6">
-                <Button/>
+                <SubmitButton pendingLabel='updating...'>
+                    Update profile
+                </SubmitButton>
             </div>
         </form>
     );
 }
 
-function Button(){
-   const {pending} = useFormStatus()
-
-    return(
-        <button
-            disabled={pending}
-            className="bg-accent-500 px-8 py-4 text-primary-800 font-semibold hover:bg-accent-600 transition-all disabled:cursor-not-allowed disabled:bg-gray-500 disabled:text-gray-300">
-            {pending ?  'Updating...' : 'Update profile'}
-        </button>
-    )
-}
 
 export default UpdateProfileForm;
--- app/_lib/actions.js
+++ app/_lib/actions.js
@@ -4,6 +4,7 @@ import {auth, signIn, signOut} from "@/app/_lib/auth";
 import {supabase} from "@/app/_lib/supabase";
 import {revalidatePath} from "next/cache";
 import {getBookings} from "@/app/_lib/data-service";
+import {redirect} from "next/navigation";
 
 export async function signInAction(){
     await signIn('google', {redirectTo: '/account'})
@@ -57,4 +58,36 @@ export async function deleteReservationAction(bookingId){
     }
 
     revalidatePath('/account/reservations');
+}
+
+export async function updateReservationAction(formData){
+    const session = await auth();
+    if(!session) throw new Error('Not authenticated');
+
+    const guestBookings = await getBookings(session?.user?.guestId);
+    const guestBookingIds = guestBookings.map(booking => booking.id);
+
+    console.log('guestBookingIds', guestBookingIds);
+    const observations = formData.get('observations');
+    const numGuests = Number(formData.get('numGuests'));
+    const bookingId = Number(formData.get('bookingId'));
+
+    if(!guestBookingIds.includes(bookingId)) throw new Error('You cannot Edit this reservation');
+
+    const updateFields = {numGuests, observations};
+
+    const { data, error } = await supabase
+        .from('bookings')
+        .update(updateFields)
+        .eq('id', bookingId)
+        .select()
+        .single();
+
+    if (error) {
+        console.error(error);
+        throw new Error('Booking could not be updated');
+    }
+
+    revalidatePath(`/account/reservations/edit/${bookingId}`);
+    redirect(`/account/reservations/`);
 }
