d7a0f178dbc9c95e0636fe8cc1c97df1d7223ba2
ptlnik13 <nikeshvpatel@gmail.com>
2026-02-13 16:56:18 -0500

    useOptimistic hook to load UI faster

--- app/_components/DeleteReservation.js
+++ app/_components/DeleteReservation.js
@@ -1,16 +1,15 @@
 'use client'
 import {TrashIcon} from '@heroicons/react/24/solid';
-import {deleteReservationAction} from "@/app/_lib/actions";
 import {useTransition} from "react";
 import SpinnerMini from "@/app/_components/SpinnerMini";
 
-function DeleteReservation({bookingId}) {
+function DeleteReservation({bookingId, onDelete}) {
 
     const [isPending, startTransition] = useTransition();
 
     function handleDelete() {
         if (confirm('Are you sure you want to delete this reservation?')) {
-            startTransition(() => deleteReservationAction(bookingId));
+            startTransition(() => onDelete(bookingId));
         }
     }
 
--- app/account/reservations/page.js
+++ app/account/reservations/page.js
@@ -1,6 +1,6 @@
-import ReservationCard from "@/app/_components/ReservationCard";
 import {getBookings} from "@/app/_lib/data-service";
 import {auth} from "@/app/_lib/auth";
+import ReservationList from "@/app/_components/ReservationList";
 
 export const metadata = {
     title: 'Update Profile'
@@ -23,13 +23,7 @@ export default async function Page() {
                         luxury cabins &rarr;
                     </a>
                 </p>
-            ) : (
-                <ul className="space-y-6">
-                    {bookings.map((booking) => (
-                        <ReservationCard booking={booking} key={booking.id} />
-                    ))}
-                </ul>
-            )}
+            ) : (<ReservationList bookings={bookings}/>)}
         </div>
     );
 }
