303901f715cb1288e3a33e6a37b931869fbb4f45
ptlnik13 <nikeshvpatel@gmail.com>
2026-02-13 13:11:14 -0500

    Updating profile with server actions.

--- app/_components/UpdateProfileForm.js
+++ app/_components/UpdateProfileForm.js
@@ -1,17 +1,19 @@
 'use client'
 import React, {useState} from 'react';
+import {updateGuestAction} from "@/app/_lib/actions";
 
-function UpdateProfileForm({ children}) {
+function UpdateProfileForm({ children, guest}) {
     const [count, setCount] = useState(0)
-    // CHANGE
-    const countryFlag = "pt.jpg";
+    const {fullName, email, nationality, nationalID, countryFlag} = guest;
 
     return (
-        <form className="bg-primary-900 py-8 px-12 text-lg flex gap-6 flex-col">
+        <form action={updateGuestAction} className="bg-primary-900 py-8 px-12 text-lg flex gap-6 flex-col">
             <div className="space-y-2">
                 <label>Full name</label>
                 <input
                     disabled
+                    defaultValue={fullName}
+                    name='fullName'
                     className="px-5 py-3 bg-primary-200 text-primary-800 w-full shadow-sm rounded-sm disabled:cursor-not-allowed disabled:bg-gray-600 disabled:text-gray-400"
                 />
             </div>
@@ -19,6 +21,8 @@ function UpdateProfileForm({ children}) {
             <div className="space-y-2">
                 <label>Email address</label>
                 <input
+                    defaultValue={email}
+                    name='email'
                     disabled
                     className="px-5 py-3 bg-primary-200 text-primary-800 w-full shadow-sm rounded-sm disabled:cursor-not-allowed disabled:bg-gray-600 disabled:text-gray-400"
                 />
@@ -39,6 +43,7 @@ function UpdateProfileForm({ children}) {
             <div className="space-y-2">
                 <label htmlFor="nationalID">National ID number</label>
                 <input
+                    defaultValue={nationalID}
                     name="nationalID"
                     className="px-5 py-3 bg-primary-200 text-primary-800 w-full shadow-sm rounded-sm"
                 />
--- app/_lib/actions.js
+++ app/_lib/actions.js
@@ -1,6 +1,7 @@
 'use server'
 
-import {signIn, signOut} from "@/app/_lib/auth";
+import {auth, signIn, signOut} from "@/app/_lib/auth";
+import {supabase} from "@/app/_lib/supabase";
 
 export async function signInAction(){
     await signIn('google', {redirectTo: '/account'})
@@ -8,4 +9,27 @@ export async function signInAction(){
 
 export async function signOutAction(){
     await signOut({redirect: false });
+}
+
+export async function updateGuestAction(formData){
+    const session = await auth();
+   if(!session) throw new Error('Not authenticated');
+
+   const fullName = formData.get('fullName');
+   const nationalID = formData.get('nationalID');
+   const email = formData.get('email');
+   const [nationality, countryFlag] = formData.get('nationality').split('%');
+
+   if(!/^[a-zA-Z0-9]{6,12}$/.test(nationalID)) throw new Error('National ID must be alphanumeric');
+
+   const updateData = {nationality, countryFlag, nationalID}
+
+    const { data, error } = await supabase
+        .from('guests')
+        .update(updateData)
+        .eq('id', session.user.guestId);
+
+    if (error) {
+        throw new Error('Guest could not be updated');
+    }
 }
