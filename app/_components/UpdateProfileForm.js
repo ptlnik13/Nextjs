34093d9cf1c7c2b292541e8c0b721c8869d232b2
ptlnik13 <nikeshvpatel@gmail.com>
2026-02-13 14:13:37 -0500

    useFormStatus for button states

--- app/_components/UpdateProfileForm.js
+++ app/_components/UpdateProfileForm.js
@@ -1,6 +1,7 @@
 'use client'
 import React, {useState} from 'react';
 import {updateGuestAction} from "@/app/_lib/actions";
+import { useFormStatus} from "react-dom";
 
 function UpdateProfileForm({ children, guest}) {
     const [count, setCount] = useState(0)
@@ -50,12 +51,22 @@ function UpdateProfileForm({ children, guest}) {
             </div>
 
             <div className="flex justify-end items-center gap-6">
-                <button className="bg-accent-500 px-8 py-4 text-primary-800 font-semibold hover:bg-accent-600 transition-all disabled:cursor-not-allowed disabled:bg-gray-500 disabled:text-gray-300">
-                    Update profile
-                </button>
+                <Button/>
             </div>
         </form>
     );
+}
+
+function Button(){
+   const {pending} = useFormStatus()
+
+    return(
+        <button
+            disabled={pending}
+            className="bg-accent-500 px-8 py-4 text-primary-800 font-semibold hover:bg-accent-600 transition-all disabled:cursor-not-allowed disabled:bg-gray-500 disabled:text-gray-300">
+            {pending ?  'Updating...' : 'Update profile'}
+        </button>
+    )
 }
 
 export default UpdateProfileForm;
